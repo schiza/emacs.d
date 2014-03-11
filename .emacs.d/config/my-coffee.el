f6322ec0dbf518d82d6fa9bbf3107712e1e2fb29
Jakub Wojtysiak <schizunio@gmail.com>
2014-03-11 17:07:37 +0100

    Make coffee-mode indentation work, after package update

--- .emacs.d/config/my-coffee.el
+++ .emacs.d/config/my-coffee.el
@@ -1,12 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
 
-;; coffee-mode custom config
-(defun coffee-custom ()
-  "coffee-mode-hook"
-  (make-local-variable 'tab-width)
-  (set 'tab-width 2))
-
-(add-hook 'coffee-mode-hook 'coffee-custom)
+(custom-set-variables '(coffee-tab-width 2))
 
 (provide 'my-coffee)
